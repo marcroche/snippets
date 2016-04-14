6a82e0c5fb0491d6f1e22f836f66043d2bfc0762
Marc Roche <marcroche@users.noreply.github.com>
2016-04-14 15:07:07 -0400

    Add question 3 file

---
+++ o2.3.js
@@ -0,0 +1,31 @@
+// This is a mock database implementation with just a connect function
+// db.connect will need to be called a total of 10 times before it successfully connects
+var counter = 0;
+var db = {
+  connect: function(cb) {
+    console.log('connection attempt' , counter + 1);
+    if (counter < 9) {
+      counter++;
+      return cb('db not ready yet');
+    }
+    return cb();
+  }
+};
+
+// Try to connect, log a successful connection & exit
+// If we fail to connect, log an error and try again with exponential backoff
+var delay = 0;
+function connectToDb() {
+  db.connect(function (err) {
+    if (err) {
+      console.error(err);
+      setTimeout(function() {
+          connectToDb();
+        }, (delay = Math.max(delay *= 2, 1)) * 1000);
+    } else {
+      console.log ('successfully connected!');
+    }
+  });  
+}
+
+connectToDb();
